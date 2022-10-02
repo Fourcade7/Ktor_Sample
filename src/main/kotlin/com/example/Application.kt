b10ef42884a794ffcc7c68d105ff0e71e69e8942
Fourcade7 <fourcadeaminov7@gmail.com>
2022-10-03 02:02:35 +0500

    Basic Routing

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -3,9 +3,23 @@ package com.example
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
+        install(Routing){
+            homeRoute()
+        }
+
     }.start(wait = true)
+}
+
+
+
+fun Routing.homeRoute(){
+    get("/pr") {
+        call.respond("Hello PrKTOR")
+    }
 }
